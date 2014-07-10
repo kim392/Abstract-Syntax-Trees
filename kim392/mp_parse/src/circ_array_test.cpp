65c32fcd4481f66e3d7f35ce74b4879e512c8c42
kim392 <kim392@7c9d8b87-a243-4a16-bb54-15c73409f4d7>
2014-07-10 03:00:22 +0000

    mp_parse.1 submission

--- kim392/mp_parse/src/circ_array_test.cpp
+++ kim392/mp_parse/src/circ_array_test.cpp
@@ -15,35 +15,7 @@ int main()
     circ_array<int> arr{6};
     for (uint64_t i = 0; i < arr.size(); ++i)
         arr[i] = i + 1;
-    arr.push_front(0);
     arr.push_back(7);
-
-    for (uint64_t i = 0; i < arr.size(); ++i)
-        assert(arr.at(i) == i);
-    assert(arr.size() == 8);
-    arr.pop_front();
-    assert(arr[0] == 1);
-    arr.pop_front();
-    assert(arr[0] == 2);
-
-    arr.pop_back();
-    assert(arr[arr.size() - 1] == 6);
-    arr.pop_back();
-    assert(arr[arr.size() - 1] == 5);
-    assert(arr.size() == 4);
-    arr.erase(2);
-
-
-    assert(arr[0] == 2);
-    assert(arr[1] == 3);
-    assert(arr[2] == 5);
-    assert(arr.size() == 3);
-
-//////////////////////////////
-arr.pop_back();
-arr.pop_back();
-arr.pop_back();
-assert(arr.size()==0);
-
+    std::cout << arr[6] << std::endl;
     return 0;
 }
